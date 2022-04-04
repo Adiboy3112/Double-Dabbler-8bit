b6f3fcbe66f5a2bdb383382457082ffd876b4c42
Adiboy <adiboy_3112@kgpian.iitkgp.ac.in>
2022-04-04 10:38:58 +0530

    changed shift register desc

--- shift_register.v
+++ shift_register.v
@@ -1,18 +1,5 @@
-// Code your design here
-module shift_register  (  input d,                      // Declare input for data to the first flop in the shift register
-                                        input clk,                    // Declare input for clock to all flops in the shift register
-                                        input en,                     // Declare input for enable to switch the shift register on/off
-                                        input rstn,         
-                                        input adda,          // Declare input to reset the register to a default value
-                                        output reg [7:0] out,
-                                        output cout);    // Declare output to read out the current value of all flops in this register
-
-
-   // This always block will "always" be triggered on the rising edge of clock
-   // Once it enters the block, it will first check to see if reset is 0 and if yes then reset register
-   // If no, then check to see if the shift register is enabled
-   // If no => maintain previous output. If yes, then shift based on the requested direction
-
+// this register is a shift and add register
+module shift_register( input d,input clk,input en,input rstn,input adda,output reg [7:0] out, output cout);   
 assign cout=out[7];
    always @ (posedge clk)
       if (!rstn)
@@ -31,7 +18,5 @@ assign cout=out[7];
                  /* code */
                  out[7:4]<=out[7:4]+3;
                  end
-
-        
       end
 endmodule
